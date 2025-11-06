4bc4ed35a12ac16468731bdd5b6b23c58ce917f6
Jackson Wozniak <jjw30@comcast.net>
2025-11-06 14:18:27 -0500

    initial server commit

---
+++ Backend/src/Core/Base/BaseException.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Core.Base;
+
+public class BaseException : Exception
+{
+    public HttpStatusCode Status { get; set; }
+
+    protected BaseException(string message, HttpStatusCode status) : base(message)
+    {
+        Status = status;
+    }
+
+    public JsonResult Json()
+    {
+        return new JsonResult(this);
+    }
+}
---
+++ Backend/src/Core/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+using Backend.Core.Base;
+
+namespace Backend.Core.Exceptions;
+
+public class NotFoundException(string message) 
+    : BaseException(message, HttpStatusCode.NotFound)
+{
+    
+}

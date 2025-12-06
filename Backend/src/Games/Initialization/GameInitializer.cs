5c7a0b5e505eb5a6169e635561fcf0b26a20d8e8
Jackson Wozniak <jjw30@comcast.net>
2025-12-05 20:17:41 -0500

    initialize games on startup if they have not already been created

--- Backend/src/Games/Initialization/GameInitializer.cs
+++ Backend/src/Games/Initialization/GameInitializer.cs
@@ -1,20 +1,30 @@
 ﻿using Backend.Games.Definitions;
+using Backend.Games.Entities;
 using Backend.Games.Repositories;
 
 namespace Backend.Games.Initialization;
 
 public class GameInitializer(IServiceProvider serviceProvider) : BackgroundService
 {
-    //retrieve definitions for each game
-    //save game, metrics etc.
+    private static readonly Dictionary<string, Game> Games = new()
+    {
+        {"Grid Recall", GridRecallDefinition.Get()}
+    };
+    
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         if (!stoppingToken.IsCancellationRequested)
         {
             using var scope = serviceProvider.CreateScope();
             var gameRepository = scope.ServiceProvider.GetRequiredService<GameRepository>();
-            
-            await gameRepository.SaveAsync(GridRecallDefinition.Get());
+
+            foreach (KeyValuePair<string, Game> game in Games)
+            {
+                if (gameRepository.FindByName(game.Key) is null)
+                {
+                    await gameRepository.SaveAsync(game.Value);
+                }
+            }
         }
     }
 }
